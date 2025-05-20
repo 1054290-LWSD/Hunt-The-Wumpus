57ded264b55e62dbed6a4895ad1f48f94cc503d4
Quinn Brodie <1054290@lwsd.org>
2025-05-19 22:38:52 -0700

    Made Tooltips work

--- Assets/Scripts/Inventory Stuff/ClickHandler.cs
+++ Assets/Scripts/Inventory Stuff/ClickHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ClickHandler : MonoBehaviour, IPointerClickHandler
 {
@@ -14,12 +15,67 @@ public class ClickHandler : MonoBehaviour, IPointerClickHandler
     [Header("Other Scripts")]
     public InventorySlot otherSlot;
 
+    [Header("Tooltip UI")]                           // ← NEW
+    public GameObject tooltipObject;
+    public Text tooltipText;
+
+    public float tooltipTimer = 0f;
+    private Vector2 offset = new Vector2(160f, 15f);
 
     RectTransform rt;
 
     void Awake()
     {
         rt = GetComponent<RectTransform>();
+        if (tooltipObject != null)
+        {
+            tooltipObject.SetActive(false); // ensure tooltip is hidden at start
+        }
+    }
+
+    void Update()
+    {
+        tooltipTimer -= Time.deltaTime;
+        if (tooltipObject != null && tooltipTimer >= 0f)
+            {
+                if (tooltipObject.activeSelf)
+                {
+                    tooltipObject.SetActive(false);
+                }
+            }
+        if (RectTransformUtility.RectangleContainsScreenPoint(rt, Input.mousePosition))
+        {
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, Input.mousePosition, null, out localPoint);
+            Rect rect = rt.rect;
+            localPoint += new Vector2(rect.width / 2, rect.height / 2);
+            bool isTopLeft = localPoint.x + localPoint.y < rect.height;
+
+            InventorySlot target = ((trianglePart == TrianglePart.TopLeft && isTopLeft)
+                                    || (trianglePart == TrianglePart.BottomLeft && !isTopLeft))
+                                    ? thisSlot
+                                    : otherSlot;
+
+            if (tooltipObject != null && tooltipText != null && target.myItem != null)
+            {
+                tooltipTimer = 2f;
+                tooltipObject.SetActive(true);
+                tooltipText.text = target.myItem.myItem.description;
+    
+                // Convert mouse position to local point in the canvas
+                RectTransform canvasRect = tooltipObject.transform.root.GetComponent<RectTransform>();
+                Vector2 localPointDeep;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    canvasRect,
+                    Input.mousePosition + (Vector3)offset,
+                    null,
+                    out localPointDeep
+                );
+
+                tooltipObject.GetComponent<RectTransform>().anchoredPosition = localPointDeep;
+
+            }
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -46,14 +102,12 @@ public class ClickHandler : MonoBehaviour, IPointerClickHandler
         }
         else
         {
-            
             Debug.Log("Other");
             if (otherSlot.myItem != null)
             {
                 otherSlot.myItem.Click(eventData);
-                
             }
-             else
+            else
             {
                 otherSlot.Click(eventData);
             }
